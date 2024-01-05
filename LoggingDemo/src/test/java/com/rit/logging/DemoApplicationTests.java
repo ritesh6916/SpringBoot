325bed9e517ada89966aa1ab336edb929b8ed81b
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-01-05 19:40:16 +0530

    default code

---
+++ LoggingDemo/src/test/java/com/rit/logging/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rit.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
